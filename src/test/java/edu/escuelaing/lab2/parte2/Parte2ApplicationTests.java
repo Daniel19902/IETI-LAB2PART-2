665c7fb2561d43534026fdf7aa677360694e768b
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-01-31 18:42:09 -0500

    lab2 parte2

---
+++ src/test/java/edu/escuelaing/lab2/parte2/Parte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.lab2.parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
